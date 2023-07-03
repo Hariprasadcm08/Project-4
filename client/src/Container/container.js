31711a5e9d7f68f7e683c0c28c2dabc09faa21d1
Hariprasadcm08 <hariprasadcm08@gmail.com>
2023-07-03 20:21:11 +0530

    updated notification

--- client/src/Container/container.js
+++ client/src/Container/container.js
@@ -1,38 +1,49 @@
 import React, { useState } from 'react';
 import './container.css';
 import axios from 'axios';
-
+import {ToastContainer ,toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function Container() {
   const [url, setUrl] = useState('');
   const [longUrl, setLongUrl] = useState('');
-  const [copied, setCopied] = useState(false);
+  // const [copied, setCopied] = useState(false);
 
+
+  const toastOption ={
+    autoClose:600,
+    pauseOnHover:true
+
+ }
   const handleCopyClick = () => {
     const inputField = document.getElementById('shortUrl');
     inputField.select();
     if (inputField.value.trim() === '') {
-      alert('Field is empty');
+     
+      toast("Field is empty",toastOption)
       return;
     }
   
     document.execCommand('copy');
-    setCopied(true);
-    setTimeout(() => {
-      setCopied(false);
-    }, 500); 
-  };
+    //setCopied(true);
+    toast("Link Copied",toastOption)
+  //   setTimeout(() => {
+  //     setCopied(false);
+  //   }, 500); 
+   };
 
   const fetchData = async () => {
     try {
       
       if (longUrl.trim() === '') {
-        alert('Please enter a URL to shorten');
+        // alert('Please enter a URL to shorten');
+        toast("Please enter a URL to shorten",toastOption)
         return;
       }
       const urlRegex = /^(https?:\/\/)?(?:w{3}\.)?([^\s\/$.?#]+\.[a-zA-Z]{2,3})(\/[^\s]*)?$/;
 
       if (!urlRegex.test(longUrl)) {
-        alert('Invalid URL format. Please enter a valid URL to shorten.');
+        //alert('Invalid URL format. Please enter a valid URL to shorten.');
+        toast("Invalid URL format. Please enter a valid URL to shorten",toastOption)
         return;
       }
       const response = await axios.post('http://localhost:5000/url/shorten', { longUrl });
@@ -51,13 +62,27 @@ function Container() {
       Shorten URL
     </button>
     <div className="notification">
-      {copied && <p id="noti"><b>Link-copied</b></p>}
+      {/* {copied && <p id="noti"><b>Link-copied</b></p>} */}
     </div>
     <input id="shortUrl" className="shortUrl" value={url} />
     <button className="copyShortUrl" onClick={handleCopyClick}>
       Copy URL
     </button>
   </div>
+  <ToastContainer
+  position="top-right"
+  autoClose={5000}
+  hideProgressBar={false}
+  newestOnTop={false}
+  closeOnClick
+  rtl={false}
+  pauseOnFocusLoss
+  draggable
+  pauseOnHover
+  theme="dark"
+  
+  
+  />
 </div>
 
   );
